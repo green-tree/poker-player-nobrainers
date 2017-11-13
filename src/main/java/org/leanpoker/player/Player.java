6e46152f68d7366710fb7579ab8ee61af25d82b6
AGruenbaum <green-tree>
2017-11-13 14:06:12 +0100

    mitgehen

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,6 +1,7 @@
 package org.leanpoker.player;
 
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 import java.util.Map;
 
@@ -9,11 +10,21 @@ public class Player {
     static final String VERSION = "NoBrainer Player";
 
     public static int betRequest(JsonElement request) {
-    	//if(request.getAsJsonObject())
-//    	request.get
-//    	current_buy_in - players[in_action][bet] + minimum_raise
-//    	request.set
-        return 112;
+    	JsonObject json = request.getAsJsonObject();
+    	JsonElement currentBuyInElement = json.get("current_buy_in");
+    	return currentBuyInElement.getAsInt();
+//    	rameter("game_state");
+//    	if(request.ge)JSONObject json = new JSONObject(yourdata);
+//    	String statistics = json.getString("statistics");
+//    	String name1 = json.getString("John");
+//    	String ageJohn = name1.getString("Age");
+//    	
+//    	String gameState = request.getasjgetPa
+//    	//if(request.getAsJsonObject())
+////    	request.get
+////    	current_buy_in - players[in_action][bet] + minimum_raise
+////    	request.set
+//        return 112;
     }
 
     public static void showdown(JsonElement game) {
