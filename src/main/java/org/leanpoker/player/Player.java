91910c9aff453aa5e25a918216734cddefa12d5d
AGruenbaum <AGruenbaum@ADC-NB11087.intern.cube.ch>
2017-11-13 11:33:39 +0100

    return 1 instead of 0

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -9,9 +9,10 @@ public class Player {
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
-        return 0;
+        return 1;
     }
 
     public static void showdown(JsonElement game) {
+    	//
     }
 }
