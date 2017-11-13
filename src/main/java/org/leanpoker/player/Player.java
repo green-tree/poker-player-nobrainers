c75c0e19871cabe5025ed433a6f5aac1a3160c15
AGruenbaum <green-tree>
2017-11-13 16:57:50 +0100

    devensiver spieler

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -31,18 +31,27 @@ public class Player {
 		String secondCardRank = cardsArray.get(1).getAsJsonObject().get("rank").getAsString();
 		//"hole_cards":[{"rank":"10","suit":"spades"}]
 		if(firstCardRank.equals(secondCardRank)) {
+			//bei zwei gleichen karten gehen wir immer all in
+			return 1000;
+		}
+		if(shouldGoAllIn(firstCardRank,secondCardRank)) {
 			return 1000;
 		}
 		
 		if(isNumber(firstCardRank) && isNumber(secondCardRank))
 		{
 			int first = getNumber(firstCardRank);
-			return 0;
+			int second = getNumber(secondCardRank);
+			if(first <= 7 && second <= 7) {
+				//wenn beide zahlen kleiner gleich 7 steigen wir immer aus!
+				return 0;
+			}
 		}
 		
 		
 		}
-    	return currentBuyInElement.getAsInt();
+		return 0;
+    	//return currentBuyInElement.getAsInt();
 //    	
 //    	String gameState = request.getasjgetPa
 //    	//if(request.getAsJsonObject())
@@ -52,6 +61,26 @@ public class Player {
 //        return 112;
     }
 
+    
+    
+     static boolean shouldGoAllIn(String firstCardRank, String secondCardRank) {
+    	 if(
+    			 (firstCardRank.equals("A") && secondCardRank.equals("K")) ||
+    			 (firstCardRank.equals("K") && secondCardRank.equals("A")) ||
+
+    			 (firstCardRank.equals("A") && secondCardRank.equals("Q")) ||
+    			 (firstCardRank.equals("Q") && secondCardRank.equals("A")) ||
+
+    			 (firstCardRank.equals("A") && secondCardRank.equals("J")) ||
+    			 (firstCardRank.equals("J") && secondCardRank.equals("A")) ||
+
+    			(firstCardRank.equals("K") && secondCardRank.equals("Q")) ||
+    			 (firstCardRank.equals("Q") && secondCardRank.equals("K")) ||
+    			 (firstCardRank.equals("A") || secondCardRank.equals("A")) ) {
+    		 return true;
+    	 }
+    	 return false;
+     }
 	private static int getNumber(String rank) {
 		return Integer.parseInt(rank);
 	}
